45f95789d67a9e552a0a5415920d5156797c6338
KevinB-sys <pkevinbarragan@gmail.com>
2025-05-02 22:52:06 -0500

    Arreglo de footer, mas bonito

--- frontend/mi-proyecto/src/components/Footer.jsx
+++ frontend/mi-proyecto/src/components/Footer.jsx
@@ -1,10 +1,92 @@
 import React from 'react';
-import '../estilos css/App.css';
+import { Facebook, Instagram, Mail, Phone, MapPin } from 'lucide-react';
+import '../estilos css/footer.css';
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>© 2025 todos los derechos reservados.</p>
+      <div className="footer-grid">
+        
+        {/* Columna 1: Logo y descripción */}
+        <div className="footer-column">
+          <div className="brand-info">
+            <div className="logo-wrapper">
+              <img 
+                src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEh8Hz0UUXxZm21BEiTgWaMMgmjHU0AKmWpwLck_ucBy2J0a2MlNaZbEeQ74sc_2-zxiqEPp3wpx43jHoEqKCWbbuwSXwSv3ihs9R2fSSh5K7-5nWVYtT6gSPG30_9qUoAZeFld2uCRcGiRmh8UD5QUH_jEzOGDncZluQbi6pnmYdjVDZIM0vmHDhetO0xE/s320/logo.png" 
+                alt="D'PET" 
+                className="footer-logo"
+              />
+              <span className="brand-name">D'PET</span>
+            </div>
+            <p className="brand-description">
+              Clínica veterinaria especializada en el cuidado integral de tus mascotas.
+            </p>
+          </div>
+        </div>
+
+        {/* Columna 2: Enlaces útiles */}
+        <div className="footer-column">
+          <h3 className="column-title">Servicios</h3>
+          <ul className="footer-links">
+            <li><a href="#">Consultas médicas</a></li>
+            <li><a href="#">Vacunación</a></li>
+            <li><a href="#">Cirugías</a></li>
+            <li><a href="#">Estética canina</a></li>
+            <li><a href="#">Emergencias 24h</a></li>
+          </ul>
+        </div>
+
+        {/* Columna 3: Contacto */}
+        <div className="footer-column">
+          <h3 className="column-title">Contacto</h3>
+          <ul className="contact-info">
+            <li>
+              <Phone size={18} />
+              <span>(593) 963 854 380</span>
+            </li>
+            <li>
+              <Mail size={18} />
+              <span>Dr. Daniel Aldaz</span>
+            </li>
+            <li>
+              <MapPin size={18} />
+              <span>Av. Dr. Pio Jaramillo Alvarado y Av.25 de Julio frente a CNT, Guayquil</span>
+            </li>
+          </ul>
+        </div>
+
+        {/* Columna 4: Redes sociales */}
+        <div className="footer-column">
+          <h3 className="column-title">Síguenos</h3>
+          <div className="social-links">
+            <a href="#" className="social-link">
+              <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M12 0C8.74 0 8.333.015 7.053.072 5.775.132 4.905.333 4.14.63c-.789.306-1.459.717-2.126 1.384S.935 3.35.63 4.14C.333 4.905.131 5.775.072 7.053.012 8.333 0 8.74 0 12s.015 3.667.072 4.947c.06 1.277.261 2.148.558 2.913.306.788.717 1.459 1.384 2.126.667.666 1.336 1.079 2.126 1.384.766.296 1.636.499 2.913.558C8.333 23.988 8.74 24 12 24s3.667-.015 4.947-.072c1.277-.06 2.148-.262 2.913-.558.788-.306 1.459-.718 2.126-1.384.666-.667 1.079-1.338 1.384-2.126.296-.765.499-1.636.558-2.913.06-1.28.072-1.687.072-4.947s-.015-3.667-.072-4.947c-.06-1.277-.262-2.149-.558-2.913-.306-.789-.718-1.459-1.384-2.126C20.408 1.417 19.736 1 18.95.694c-.765-.297-1.636-.499-2.913-.558C15.667.012 15.26 0 12 0zm0 2.16c3.203 0 3.585.016 4.85.071 1.17.055 1.805.249 2.227.415.562.217.96.477 1.382.896.419.42.679.819.896 1.381.164.422.36 1.057.413 2.227.057 1.266.07 1.646.07 4.85s-.015 3.585-.074 4.85c-.061 1.17-.256 1.805-.421 2.227-.224.562-.479.96-.899 1.382-.419.419-.824.679-1.38.896-.42.164-1.065.36-2.235.413-1.274.057-1.649.07-4.859.07-3.211 0-3.586-.015-4.859-.074-1.171-.061-1.816-.256-2.236-.421-.569-.224-.96-.479-1.379-.899-.421-.419-.69-.824-.9-1.38-.165-.42-.359-1.065-.42-2.235-.045-1.26-.061-1.649-.061-4.844 0-3.196.016-3.586.061-4.861.061-1.17.255-1.814.42-2.234.21-.57.479-.96.9-1.381.419-.419.81-.689 1.379-.898.42-.166 1.051-.361 2.221-.421 1.275-.045 1.65-.06 4.859-.06l.045.03zm0 3.678c-3.405 0-6.162 2.76-6.162 6.162 0 3.405 2.76 6.162 6.162 6.162 3.405 0 6.162-2.76 6.162-6.162 0-3.405-2.76-6.162-6.162-6.162zM12 16c-2.21 0-4-1.79-4-4s1.79-4 4-4 4 1.79 4 4-1.79 4-4 4zm7.846-10.405c0 .795-.646 1.44-1.44 1.44-.795 0-1.44-.646-1.44-1.44 0-.794.646-1.439 1.44-1.439.793-.001 1.44.645 1.44 1.439z" fill="white"/>
+              </svg>
+            </a>
+            <a href="#" className="social-link">
+              <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" fill="white"/>
+              </svg>
+            </a>
+            <a href="#" className="social-link">
+              <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M12.525.02c1.31-.02 2.61-.01 3.91-.02.08 1.53.63 3.09 1.75 4.17 1.12 1.11 2.7 1.62 4.24 1.79v4.03c-1.44-.05-2.89-.35-4.2-.97-.57-.26-1.1-.59-1.62-.93-.01 2.92.01 5.84-.02 8.75-.08 1.4-.54 2.79-1.35 3.94-1.31 1.92-3.58 3.17-5.91 3.21-1.43.08-2.86-.31-4.08-1.03-2.02-1.19-3.44-3.37-3.65-5.71-.02-.5-.03-1-.01-1.49.18-1.9 1.12-3.72 2.58-4.96 1.66-1.44 3.98-2.13 6.15-1.72.02 1.48-.04 2.96-.04 4.44-.99-.32-2.15-.23-3.02.37-.63.41-1.11 1.04-1.36 1.75-.21.51-.15 1.07-.14 1.61.24 1.64 1.82 3.02 3.5 2.87 1.12-.01 2.19-.66 2.77-1.61.19-.33.4-.67.41-1.06.1-1.79.06-3.57.07-5.36.01-4.03-.01-8.05.02-12.07z" fill="white"/>
+              </svg>
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div className="footer-bottom">
+        <div className="copyright">
+          © {new Date().getFullYear()} D'PET. Todos los derechos reservados.
+        </div>
+        <div className="legal-links">
+          <a href="#">Política de privacidad</a>
+          <a href="#">Términos de servicio</a>
+        </div>
+      </div>
     </footer>
   );
 };
